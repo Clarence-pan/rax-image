4a2b09f7dc66738ac890385899a9274686742278
黄昱 <marc.hy@alibaba-inc.com>
2019-06-21 16:29:08 +0800

    update mini app image component

--- src/miniapp/index.ts
+++ src/miniapp/index.ts
@@ -1,21 +1,60 @@
-'use strict';
 import fmtEvent from './fmtEvent';
 
 Component({
-  data: {},
+  data: {
+    mode: "aspectFit"
+  },
   props: {
-    className: '',
-    style: '',
+    className: "",
+    style: "",
     source: {
-      uri: ''
+      uri: ""
     },
+    resizeMode: "contain",
+    lazyLoad: false,
     onClick: function onClick() {}
   },
-  didMount: function didMount() {},
+  onInit() {
+    this.initImage();
+  },
+  didMount: function didMount() {
+    if(!my.canIUse('component2')) {
+      this.initImage();
+    }
+  },
   methods: {
     onClick: function onClick(e) {
       var event = fmtEvent(this.props, e);
       this.props.onClick(event);
+    },
+    initImage: function initImage(e) {
+      var mode = 'aspectFit';
+      const { width=null, height=null } = this.props.source || {};
+      let style = this.props.style || '';
+      if (width) style += 'width:' + width + 'rpx;';
+      if (height) style += 'height:' + height + 'rpx;';
+
+      switch (this.props.resizeMode) {
+        case 'cover':
+          mode = 'aspectFill';
+          break;
+
+        case 'contain':
+          mode = 'aspectFit';
+          break;
+
+        case 'stretch':
+          mode = 'scaleToFill';
+          break;
+
+        default:
+          mode = 'aspectFit';
+      }
+
+      this.setData({
+        mode: mode,
+        style: style
+      });
     }
   }
 });
